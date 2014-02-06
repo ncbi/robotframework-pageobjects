7a56738e041f040fe28487bca990e746dd9b4ab2
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-02-06 11:58:03 -0500

    DCLT-648: renamed a keyword to make more readable

--- page_objects/PubmedPageLibrary.py
+++ page_objects/PubmedPageLibrary.py
@@ -1,4 +1,4 @@
-from EntrezPageLibrary import EntrezPageLibrary
+from EntrezPageLibrary import robot_alias, EntrezPageLibrary
 from BooksPageLibrary import BooksPageLibrary
 
 
@@ -6,6 +6,7 @@ class PubmedPageLibrary(EntrezPageLibrary):
     name = "pubmed"
     homepage = "http://www.ncbi.nlm.nih.gov/pubmed"
 
+    @robot_alias("find_related_data_from__name__in")
     def find_related_data_in(self, dbname):
         self.se.wait_until_element_is_visible("rdDatabase")
         self.se.select_from_list_by_value("rdDatabase", dbname)
