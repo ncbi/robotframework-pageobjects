250b825cfaa667930e7f934e28c608b4f3d9d563
Daniel Frishberg <frishber@ncbi.nlm.nih.gov>
2014-02-06 11:58:55 -0500

    foo

--- examples/getrm/GeTRM.py
+++ examples/getrm/GeTRM.py
@@ -1,14 +1,22 @@
-from Selenium2Library import Selenium2Library
-class GeTRM(Selenium2Library):
+import sys
+from page_objects.PageObjectLibrary import PageObjectLibrary
+#from Selenium2Library import Selenium2Library
+class GeTRM(PageObjectLibrary):
+    browser = "firefox"
+    homepage =  "http://www.ncbi.nlm.nih.gov/variation/tools/get-rm"
     def clear_element(self, locator):
-        element = self._element_find(locator, True, True)
+        element = self.se._element_find(locator, True, True)
         element.clear()
         
     def get_values(self, locator, tag=None):
-        element = self._elements_find(locator, False, False, tag=tag)
+        element = self.se._elements_find(locator, False, False, tag=tag)
         if element is None:
             return None
         elif not isinstance(element, list) and not isinstance(element, tuple):
             return element.get_attribute("value")
         else:
             return reduce(lambda val1, val2: val1 + val2, [el.get_attribute("value") for el in element])
+    
+    def __init__(self, *args, **kwargs):
+        super(GeTRM, self).__init__(self, *args, **kwargs)
+        print self.open
--- examples/getrm/test_getrm.py
+++ examples/getrm/test_getrm.py
@@ -1,5 +1,5 @@
 import unittest
-from Selenium2Library import Selenium2Library
+from GeTRM import GeTRM
 
 
 expected_vals = { "1q24" : "Homo sapiens: GRCh37.p\d+\s+Chr\s1\s\WNC_000001.\d+\W:\s164.7\d+M\s-\s173.6\d+M",
@@ -15,30 +15,34 @@ selectors = {
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.s2l = Selenium2Library()
-        self.s2l.open_browser("http://www.ncbi.nlm.nih.gov/variation/tools/get-rm", "ff")
+        self.se = GeTRM()
+        self.se.open_browser("http://www.ncbi.nlm.nih.gov/variation/tools/get-rm", "ff")
     def find(self, selector):
         locator = selectors[selector]
-        return self.s2l._element_find(locator, True, True)
+        return self.se._element_find(locator, True, True)
     
+    def search(self, term):
+        rm_search_box = self.find("search")
+        rm_search_box.clear()
+        rm_search_box.send_keys(term)
+        #if os.name == "posix":
+        #rm_search_box.send_keys("ENTER") # Might need to substitute here
+        #else:
+        self.se.click_element(selectors["search-arrow"])
+
+    def go_to_results(self):
+        self.se.wait_until_page_contains_element(selectors["result-arrow"])
+        #self.se.mouse_over(selectors["result-arrow"])
+        self.se.click_element(selectors["result-arrow"])
+
     def test_foo(self):
         for term, expected_val in expected_vals.iteritems():
-            rm_search_box = self.find("search")
-            rm_search_box.clear()
-            rm_search_box.send_keys(term)
-            if os.name == "posix":
-                rm_search_box.send_keys("ENTER") # Might need to substitute here
-            else:
-                self.se.click_element(selectors["search-arrow"])
-                content_icon.click()
-
-            self.se.wait_until_page_contains_element(selectors["result-arrow"])
-            self.se.mouse_over(selectors["result-arrow"])
-            self.se.click_element("result-arrow")
-
+            self.search(term)
+            self.go_to_results()
+            
     
     def tearDown(self):
-        self.s2l.close_browser()
+        self.se.close_browser()
 
 
 if __name__ == "__main__":
