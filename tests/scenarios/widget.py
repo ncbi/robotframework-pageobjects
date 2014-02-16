a2e1adb434f7bcb8e13a501e07b2b9164f7e6407
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-02-15 22:58:34 -0500

    DCLT-669: some more cleanup

---
+++ tests/scenarios/widget.py
@@ -0,0 +1,29 @@
+import os
+
+import robot.utils.asserts as asserts
+
+from pageobjects.base.PageObjectLibrary import PageObjectLibrary, robot_alias
+
+
+class Page(PageObjectLibrary):
+    name = "Widget Page"
+    homepage = "file://" + os.path.dirname(os.path.realpath(__file__)) + "/pages/widget-home-page.html"
+
+    @robot_alias("search__name__for")
+    def search(self, term):
+        self.input_text("q", "search term")
+        self.click_element("go")
+        return SearchResultPage()
+
+
+class SearchResultPage(Page):
+    name = "Widget Search Result Page"
+
+    @robot_alias("__name__should_have_results")
+    def should_have_results(self, expected):
+        len_results = len(self._find_element("xpath=id('results')/li", False, False))
+        asserts.assert_equals(len_results, int(expected), "Unexpected number of results found on %s, got %s, "
+                                                         "expected %s" %(
+            self.name, len_results, expected))
+
+
