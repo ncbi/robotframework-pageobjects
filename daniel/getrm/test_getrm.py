a259c390e3391894959daadc47865181ff904c42
Daniel Frishberg <frishber@ncbi.nlm.nih.gov>
2014-02-24 18:05:29 -0500

    DCLT-659: Removed change in test_getrm.py

--- daniel/getrm/test_getrm.py
+++ daniel/getrm/test_getrm.py
@@ -3,7 +3,7 @@ from nose2.tools import params
 from pageobjects.GeTRMPageLibrary import GeTRMPageLibrary as GeTRMPage
 
 
-expected_vals = (#("1q24", "Homo sapiens: GRCh37.p\d+\s+Chr\s1\s\WNC_000001.\d+\W:\s164.7\d+M\s-\s173.6\d+M"),
+expected_vals = (("1q24", "Homo sapiens: GRCh37.p\d+\s+Chr\s1\s\WNC_000001.\d+\W:\s164.7\d+M\s-\s173.6\d+M"),
                 ("Neurofibromatosis", "Homo sapiens: GRCh37.p\d+\s+Chr\s17\s\WNC_000017.\d+\W:\s29.6\d+M\s-\s29.\d+M"), 
 )
 
