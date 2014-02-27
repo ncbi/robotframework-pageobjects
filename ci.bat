a70d23db0a39bd958e86cdbc61a639ccc03f24c2
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-02-26 23:35:59 -0500

    Again trying with windows

--- ci.bat
+++ ci.bat
@@ -3,6 +3,4 @@ call virtualenv robot
 call robot\Scripts\activate.bat
 call pip install nose
 call pip install -e .
-call pip install -r requirements.txt
-call pip install inflection
 call nosetests -vs --with-xunit tests/
