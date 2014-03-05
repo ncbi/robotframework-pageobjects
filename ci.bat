c7c75b651a42463d79edfca916e72b4abd60bea3
Frishberg <frishber@msdev01.ncbi.nlm.nih.gov>
2014-03-05 17:47:36 -0500

    DCLT-700: pip freeze

--- ci.bat
+++ ci.bat
@@ -1,6 +1,7 @@
-call rmdir robot /s /q
-call virtualenv robot
-call robot\Scripts\activate.bat
-call pip install nose
-call pip install -e .
-call nosetests -vs --with-xunit tests/
+call pip freeze
+::call rmdir robot /s /q
+::call virtualenv robot
+::call robot\Scripts\activate.bat
+::call pip install nose
+::call pip install -e .
+::call nosetests -vs --with-xunit tests/
