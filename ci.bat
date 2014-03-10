199aa7001cb4c9a0a4efbf51ef5fa01b6961574d
frishber <frishber@ncbi.nlm.nih.gov>
2014-03-10 16:21:03 -0400

    DCLT-667: Put some debugging in ci.bat to look for hanging phantomjs/firefox processes

--- ci.bat
+++ ci.bat
@@ -4,4 +4,6 @@ call robot\Scripts\activate.bat
 call pip install nose
 call pip install -e .
 call pip install mock
+call tasklist /v | find "phantomjs"
+call tasklist /v | find "firefox"
 call nosetests -vs --with-xunit tests/
