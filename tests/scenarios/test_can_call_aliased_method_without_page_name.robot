a43e592f4ff6e1f9edbd85fe4af6ebfb5db70eb3
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-05-27 12:15:24 -0400

    DCLT-829: Added tests for not using page name in aliased keyword

--- tests/scenarios/test_can_call_aliased_method_without_page_name.robot
+++ tests/scenarios/test_can_call_aliased_method_without_page_name.robot
@@ -3,6 +3,4 @@ Library  keyword_naming.AliasedMethodPage
 
 *** Test Cases ***
 Can Call An Aliased Method Without Page Name
-    Open Aliased Method Page
     Do Something
-    Close Aliased Method Page
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -332,6 +332,6 @@ class KeywordsTestCase(BaseTestCase):
         run = self.run_scenario("test_does_not_return.robot", variable="baseurl:%s" % self.base_file_url)
         self.assert_run(run, expected_returncode=1, search_output="Every page object method must have a return value.")
 
-    def test_can_alias_with_page_name(self):
-        run = self.run_scenario("test_can_alias_with_page_name.robot", variable="baseurl:%s" % self.base_file_url)
+    def test_can_alias_without_page_name(self):
+        run = self.run_scenario("test_can_call_aliased_method_without_page_name.robot", variable="baseurl:%s" % self.base_file_url)
         self.assert_run(run, expected_returncode=0, search_output="PASS")
