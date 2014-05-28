d6746da3360c9495bbd52f3c2fb67b30672b82ab
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-05-28 10:50:18 -0400

    QAR-47992: Got rid of old demo

--- demo/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-When uncommented, this line allows users to import
-your page objects like this:
-    from myapppageobjects import MyAppHomePage, etc.
-"""
-
-from google import *
-from googlesearchresultcomponent import GoogleSearchResultComponentManager
