fb56d6f5bf15f0a2cfb50833c8c506cd1ba23485
Daniel Frishberg <frishber@ncbi.nlm.nih.gov>
2014-05-30 10:57:18 -0400

    DCLT-779: Applied version changes to RF

--- setup.py
+++ setup.py
@@ -4,18 +4,20 @@ import os
 from setuptools import setup, find_packages
 import subprocess
 import shlex
+import version
 
 reqs = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                     'requirements.txt')
 REQUIRES = filter(None, open(reqs).read().splitlines())
 
+ver = version.get_git_version()
 
 # Listing inflection module as requirements is not working in windows for some
 # reason. So install it using pip install.
 
 setup(
     name="Robot-Framework-Page-Objects",
-    version="0.3.1",
+    version=ver,
     description="Lets you use the page object pattern with Robot Framework and plain python", 
     author="National Center for Biotechnology Information",
     install_requires=REQUIRES,
