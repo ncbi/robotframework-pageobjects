9b89bffb958c6a163e1272270ccb1e845f32076b
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-06-11 12:56:30 -0400

    QAR-48036: Took out extra class

--- robotpageobjects/page.py
+++ robotpageobjects/page.py
@@ -372,27 +372,6 @@ class _SelectorsManager(_S2LWrapper):
         return self._element_find(locator, False, required, **kwargs)
 
 
-class _ComponentElementFinder(ElementFinder):
-    """Overrides the element finder class that SE2Lib's
-    _element_find uses so that we can pass the reference webelement
-    instead of the driver. This allows us to limit our DOM search
-    in components to the "reference webelement" instead of searching
-    globally on the driver instance.
-    """
-
-    def __init__(self, webelement):
-
-        super(_ComponentElementFinder, self).__init__()
-        self._reference_webelement = webelement
-
-    def find(self, browser, locator, tag=None):
-        prefix = self._parse_locator(locator)[0]
-        if prefix == "dom":
-            return super(_ComponentElementFinder, self).find(browser, locator, tag=tag)
-        else:
-            return super(_ComponentElementFinder, self).find(self._reference_webelement, locator, tag=tag)
-
-
 class _BaseActions(_SelectorsManager):
     """
     Helper class that defines actions for PageObjectLibrary.
