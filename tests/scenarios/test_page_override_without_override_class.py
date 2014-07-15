735f8052678c7790ee6539d7d1372d89a5c639ba
Daniel Frishberg <frishber@ncbi.nlm.nih.gov>
2014-07-15 10:32:54 -0400

    Took out print statement

--- tests/scenarios/test_page_override_without_override_class.py
+++ tests/scenarios/test_page_override_without_override_class.py
@@ -1,5 +1,4 @@
 import os
-print os.environ.get("PYTHONPATH", "NOPE")
 from result_component import ParaComponent, TwoComponentsPage
 class TwoComponentsSubPageWithoutOverride(TwoComponentsPage):
     components = {ParaComponent: "css=p:last-child"}
