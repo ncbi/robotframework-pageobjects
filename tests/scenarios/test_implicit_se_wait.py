7f8d97746734fd3d451cf714b7e9ce04df85d037
Aaron Cohen <cohenaa@ncbi.nlm.nih.gov>
2014-07-21 13:41:22 -0400

    QAR-48196: took print statement out of unit test for selectors

--- tests/scenarios/test_implicit_se_wait.py
+++ tests/scenarios/test_implicit_se_wait.py
@@ -5,9 +5,6 @@ from po.widget_rel_uri_attr import Page
 class ImplicitWaitTestCase(unittest.TestCase):
 
     def setUp(self):
-        import os
-        os.environ["PO_BROWSER"] = "firefox"
-        os.environ["PO_BASEURL"] = "file:///home/cohenaa/projects/ift/robotframework-pageobjects/tests/scenarios"
         self.p = Page()
         self.p.open()
 
