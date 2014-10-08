374f34e8c2ea878126dd6194880a317b6e9b227b
frishber <frishber@ncbi.nlm.nih.gov>
2014-10-08 10:27:13 -0400

    QAR-48321: Got tests passing

--- robotpageobjects/page.py
+++ robotpageobjects/page.py
@@ -69,7 +69,12 @@ class _PageMeta(_ComponentsManagerMeta):
         except AttributeError:
             self._keyword_depth = 1
         ret = f(*args, **kwargs)
+
         self._keyword_depth -= 1
+        # We only get to this if there's no exception in the previous line.
+        # If there is an exception, we decrement in _run_on_failure.
+        # We don't do this in a "finally" clause here, because run-on-failure
+        # needs to know how far in we are.
         return ret
 
     @classmethod
@@ -291,11 +296,21 @@ class Page(_BaseActions, _SelectorsManager, _ComponentsManager):
     def _run_on_failure(self, *args, **kwargs):
         """Check the "_keyword_depth" counter, which is incremented by the "mark_depth"
         decorator, and only call super if it is set to 1."""
-        if not hasattr(self, "_keyword_depth") or self._keyword_depth == 0:
-            # We're actually in a non-keyword that was decorated by Se2Lib.
+        import sys
+        sys.__stdout__.write("\n%s\n" % getattr(self, "_keyword_depth"))
+        if hasattr(self, "_keyword_depth") and self._keyword_depth != 0 and self._keyword_depth == 1:
+            # We only run the run-on-failure keyword if _keyword_depth is 1.
+            #  If _keyword_depth is not set, or _keyword_depth is 0, then
+            #  we're actually in a non-keyword that was decorated by Se2Lib.
             #  We know this because all keywords have the count incremented.
-            #  Don't run the run-on-failure keyword.
-            return
-        elif self._keyword_depth == 1:
+            #  If _keyword_depth > 1, then we don't need to run it because we
+            #  will handle it when we get back up the stack to 1.
             super(Page, self)._run_on_failure(*args, **kwargs)
-            self._keyword_depth = 0
+        if self._keyword_depth != 0:
+            self._keyword_depth -= 1
+            # Decrement for when we pass back up the chain.
+            #  We do this here and not in the decorator,
+            #  because we need to know the actual value here
+            #  before we decrement.
+            #  We don't decrement when it's 0, because that only
+            #  happens if we're in a non-keyword (run_keyword or get_keyword_names).
