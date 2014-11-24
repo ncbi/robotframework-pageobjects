24bcfb81ac6dcac4d774ff994a7bb6f79da795e9
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2014-11-24 12:04:27 -0500

    Changed titles of tests

--- demo/test_pubmed.py
+++ demo/test_pubmed.py
@@ -8,7 +8,7 @@ class PubmedTestCase(unittest.TestCase):
         self.pubmed_homepage = PubmedHomePage()
         self.pubmed_homepage.open()
 
-    def test_first_result_page_title_should_contain_search_term(self):
+    def test_first_result_page_body_should_contain_search_term(self):
         pubmed_docsum_page = self.pubmed_homepage.search_for("cat")
         self.article_page = pubmed_docsum_page.click_result(1)
         self.article_page.body_should_contain("cat")
