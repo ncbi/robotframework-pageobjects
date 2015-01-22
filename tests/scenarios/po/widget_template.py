c741537ce710326baba0b724cf1cd2bcfe8dfdaa
hellmanj <hellmanj@ncbi.nlm.nih.gov>
2015-01-22 14:43:37 -0500

    take out unused PO selector

--- tests/scenarios/po/widget_template.py
+++ tests/scenarios/po/widget_template.py
@@ -6,6 +6,5 @@ class WidgetItemPage(Page):
     name = "Widget Item Page"
     uri_template = "/site/category/{category}/{id}.html"
     selectors = {"title": "css=h1",
-                 "hidden-element": "css=#hidden",
-                 "see-also": "id=see-also"}
+                 "hidden-element": "css=#hidden"}
 
