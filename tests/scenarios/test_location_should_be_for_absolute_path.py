dd5b5cfd06f381e47c91f146394cfbd7cda06c78
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-03-19 10:51:13 -0400

    Cleaned up tests against www.ncbi with file scheme to mock site

--- tests/scenarios/test_location_should_be_for_absolute_path.py
+++ tests/scenarios/test_location_should_be_for_absolute_path.py
@@ -1,15 +1,17 @@
-from po.ncbi import NCBIPage
 import unittest
 import os
 
+from robotpageobjects import Page
 
 class BaseMethodLocationShouldBeTestCase2(unittest.TestCase):
+    baseurl = "file://%s" % os.path.join(os.path.dirname(os.path.realpath(__file__)), "site")
 
     def test_location_should_be_for_absolute_path(self):
-        os.environ["PO_BASEURL"] = "http://www.ncbi.nlm.nih.gov"
-        self.p = NCBIPage()
-        self.p.open({"path": "pubmed"})
-        self.p.location_should_be("http://www.ncbi.nlm.nih.gov/pubmed")
+        os.environ["PO_BASEURL"] = self.baseurl
+        Page.uri = "/index.html"
+        self.p = Page()
+        self.p.open()
+        self.p.location_should_be(os.path.join(self.baseurl, "index.html"))
 
     def tearDown(self):
         self.p.close()
