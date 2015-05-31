64ecf3882dc709c10573b07b3bd66da1639f63b9
cohenaa <cohenaa@ncbi.nlm.nih.gov>
2015-05-31 10:41:08 -0400

    Added sauce mobile test file

--- tests/scenarios/test_sauce_mobile.py
+++ tests/scenarios/test_sauce_mobile.py
@@ -6,10 +6,12 @@ import os
 class NCBITestCase(unittest.TestCase):
     def test_title_should_fail(self):
         os.environ["PO_BASEURL"] = "http://www.ncbi.nlm.nih.gov"
-        os.environ["PO_BROWSER"] = "Firefox"
+        os.environ["PO_SAUCE_PLATFORM"] = "OS X 10.10"
+        os.environ["PO_BROWSERVERSION"] = "8.2"
+        os.environ["PO_BROWSER"] = "iPhone"
+        os.environ["PO_SAUCE_DEVICE"] = "iPhone Simulator"
         os.environ["PO_SAUCE_USERNAME"] = os.getenv("SAUCE_USERNAME")
         os.environ["PO_SAUCE_APIKEY"] = os.getenv("SAUCE_APIKEY")
-        os.environ["PO_SAUCE_PLATFORM"] = "Windows 8.1"
         self.p = NCBIPage()
         self.p.open({"path": "pubmed"})
         self.p.title_should_be("Home - PubMed - NCBI")
