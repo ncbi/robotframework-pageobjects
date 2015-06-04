b7f7873a46bc9af7bcd599c7e2412a5fff77a848
hellmanj <hellmanj@ncbi.nlm.nih.gov>
2015-06-04 12:32:15 -0400

    use packit for building/installing

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-import os
-from setuptools import setup, find_packages
-import subprocess
-import shlex
-import version
-
-reqs = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                    'requirements.txt')
-REQUIRES = filter(None, open(reqs).read().splitlines())
-
-ver = version.get_git_version()
-
-# Listing inflection module as requirements is not working in windows for some
-# reason. So install it using pip install.
-
-setup(
-    name="robotframework-pageobjects",
-    version=ver,
-    description="Lets you use the page object pattern with Robot Framework and plain python", 
-    author="National Center for Biotechnology Information",
-    url="https://github.com/ncbi/robotframework-pageobjects",
-    keywords="robotframework testing testautomation selenium selenium2 webdriver web",
-    install_requires=REQUIRES,
-    packages=find_packages(exclude=("tests",)),
-    zip_safe=False
-)
+setup(setup_requires='packit', packit=True)
