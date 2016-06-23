98aea34d731968b9cae5e0592c9651ba96cfce84
Timo Stollenwerk <tisto@plone.org>
2016-06-23 14:04:55 +0200

    Fix a few relative imports.

--- robotpageobjects/tests/scenarios/po/basepageobjects/__init__.py
+++ robotpageobjects/tests/scenarios/po/basepageobjects/__init__.py
@@ -1,2 +1,2 @@
-from homepage import BaseHomePage
-from resultspage import BaseResultsPage
+from robotpageobjects.tests.scenarios.po.basepageobjects.homepage import BaseHomePage
+from robotpageobjects.tests.scenarios.po.basepageobjects.resultspage import BaseResultsPage
--- robotpageobjects/tests/scenarios/po/mydbpageobjects/__init__.py
+++ robotpageobjects/tests/scenarios/po/mydbpageobjects/__init__.py
@@ -1,2 +1,2 @@
-from homepage import MyDBHomePage
-from resultspage import MyDBResultsPage
+from robotpageobjects.tests.scenarios.po.basepageobjects.homepage import MyDBHomePage
+from robotpageobjects.tests.scenarios.po.basepageobjects.resultspage import MyDBResultsPage
